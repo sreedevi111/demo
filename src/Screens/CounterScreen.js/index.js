1bd7c301a7c3012eb8e9f662b2871f522601d2fa
Sreedevi <sreedevisobhana@gmail.com>
2022-09-01 15:17:45 +0530

    initial commit

---
+++ src/Screens/CounterScreen.js/index.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>CounterScreen</Text>
+    </View>
+  )
+}
+
+export default CounterScreen
+
+const styles = StyleSheet.create({})
---
+++ src/Screens/LandingScreen.js/index.js
@@ -0,0 +1,14 @@
+import {Text, View } from 'react-native'
+import React from 'react'
+import styles from './styles'
+
+const LandingScreen = () => {
+  return (
+    <View>
+      <Text style={styles.textline}>LandingScreen</Text>
+    </View>
+  )
+}
+
+export default LandingScreen
+
